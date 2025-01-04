38c6021e77baaa2d0217dc54209a91cdad01ee0d
Hendrik Ziegler <hendrikziegler@outlook.de>
2025-01-04 17:17:25 +0100

    [Hendrik-Leon] refactored messages into seperate package

---
+++ Hendrik-Leon/messages/messages.go
@@ -0,0 +1,189 @@
+package messages
+
+import (
+	"encoding/binary"
+	"io"
+	"net"
+	"unicode/utf8"
+)
+
+type (
+	ErrorCode byte
+	MessageID byte
+)
+
+const (
+	Error MessageID = iota
+	RegistrationRequest
+	RegistrationResponse
+	ClientListRecieved
+	NewClientConnected
+	ClientDisconnectedS2C
+	Broadcast
+	DisconnectC2S
+)
+
+const (
+	InvalidMessageID ErrorCode = iota
+	IPPortNotUnique
+	NameNotUnique
+	NameLengthZero
+	NameNotUTF8
+	InvalidClientList
+	NoError = 255
+)
+
+type ClientInfo struct {
+	Client_ip   net.IP
+	Client_port uint16
+	Name_len    byte
+	Name        string
+}
+
+func WriteClientInfo(w io.Writer, info ClientInfo) {
+	w.Write(info.Client_ip)
+	binary.Write(w, binary.BigEndian, info.Client_port)
+	binary.Write(w, binary.BigEndian, info.Name_len)
+	binary.Write(w, binary.BigEndian, []byte(info.Name))
+}
+
+// Messages
+
+type ErrorMessage struct {
+	Message_id MessageID
+	ErrorCode  ErrorCode
+}
+
+func WriteErrorMessage(w io.Writer, code ErrorCode) {
+	_, err := w.Write([]byte{byte(Error), byte(code)})
+	if err != nil {
+		panic(err)
+	}
+}
+
+type RegistrationRequestMessage struct {
+	Message_id MessageID
+	Client     ClientInfo
+}
+
+func ReadRegistrationRequestMessage(conn io.Reader) (RegistrationRequestMessage, ErrorCode) {
+	m := make([]byte, 8)
+	_, err := conn.Read(m[:1])
+	if err != nil {
+		panic(err)
+	}
+
+	if m[0] != byte(RegistrationRequest) {
+		return RegistrationRequestMessage{}, InvalidMessageID
+	}
+
+	_, err = conn.Read(m[1:5])
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = conn.Read(m[5:7])
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = conn.Read(m[7:8])
+	if err != nil {
+		panic(err)
+	}
+
+	if m[7] == 0 {
+		return RegistrationRequestMessage{}, NameLengthZero
+	}
+
+	name := make([]byte, m[7])
+	_, err = conn.Read(name)
+	if err != nil {
+		panic(err)
+	}
+
+	if !utf8.Valid(name) {
+		return RegistrationRequestMessage{}, NameNotUTF8
+	}
+
+	return RegistrationRequestMessage{
+		Message_id: MessageID(m[0]),
+		Client: ClientInfo{
+			Client_ip:   m[1:5],
+			Client_port: binary.BigEndian.Uint16(m[5:7]),
+			Name_len:    m[7],
+			Name:        string(name),
+		},
+	}, NoError
+}
+
+type RegistrationResponseMessage struct {
+	Message_id MessageID
+	N_clients  uint32
+	Clients    []ClientInfo
+}
+
+func WriteRegistrationResponse(w io.Writer, list map[net.Conn]ClientInfo) {
+	binary.Write(w, binary.BigEndian, byte(RegistrationResponse))
+	binary.Write(w, binary.BigEndian, uint32(len(list)))
+
+	for _, info := range list {
+		WriteClientInfo(w, info)
+	}
+}
+
+type BroadcastMessage struct {
+	Message_id  MessageID
+	Message_len uint16
+	Message     string
+}
+
+func WriteBroadcastMessage(w io.Writer, msg string) {
+	binary.Write(w, binary.BigEndian, byte(Broadcast))
+	binary.Write(w, binary.BigEndian, uint32(len(msg)))
+	w.Write([]byte(msg))
+}
+
+// already read message ID
+func ReadBroadcastMessage(r io.Reader) BroadcastMessage {
+	msg_len := make([]byte, 4)
+	_, err := r.Read(msg_len)
+	if err != nil {
+		panic(err)
+	}
+
+	msg_len_16 := binary.BigEndian.Uint16(msg_len)
+	msg := make([]byte, msg_len_16)
+	_, err = r.Read(msg)
+	if err != nil {
+		panic(err)
+	}
+
+	return BroadcastMessage{
+		Message_id:  Broadcast,
+		Message_len: msg_len_16,
+		Message:     string(msg),
+	}
+}
+
+type NewClientConnectedMessage struct {
+	Message_id MessageID
+	Client     ClientInfo
+}
+
+func WriteNewClientConnectedMessage(w io.Writer, client ClientInfo) {
+	binary.Write(w, binary.BigEndian, byte(NewClientConnected))
+	WriteClientInfo(w, client)
+}
+
+type ClientDisconnectedMessage struct {
+	Message_id MessageID
+	Name_len   byte
+	Name       string
+}
+
+func WriteClientDisconnectMessage(w io.Writer, name string) {
+	binary.Write(w, binary.BigEndian, byte(ClientDisconnectedS2C))
+	binary.Write(w, binary.BigEndian, byte(len(name)))
+	w.Write([]byte(name))
+}
--- Hendrik-Leon/server/server.go
+++ Hendrik-Leon/server/server.go
@@ -1,85 +1,21 @@
 package main
 
 import (
-	"encoding/binary"
 	"fmt"
 	"io"
 	"net"
 	"os"
 	"sync"
-	"unicode/utf8"
+
+	"github.com/bafto/PeerToPeer/messages"
 )
-
-type ErrorCode byte
-type MessageID byte
-
-const (
-	Error MessageID = iota
-	RegistrationRequest
-	RegistrationResponse
-	ClientListRecieved
-	NewClientConnected
-	ClientDisconnectedS2C
-	Broadcast
-	DisconnectC2S
-)
-
-const (
-	InvalidMessageID ErrorCode = iota
-	IPPortNotUnique
-	NameNotUnique
-	NameLengthZero
-	NameNotUTF8
-	InvalidClientList
-	NoError = 255
-)
-
-type ClientInfo struct {
-	client_ip   net.IP
-	client_port uint16
-	name_len    byte
-	name        string
-}
-
-// Messages
-
-type ErrorMessage struct {
-	message_id MessageID
-	errorCode  ErrorCode
-}
-
-type RegistrationRequestMessage struct {
-	message_id MessageID
-	client     ClientInfo
-}
-
-type RegistrationResponseMessage struct {
-	message_id MessageID
-	n_clients  uint32
-	clients    []ClientInfo
-}
-
-type NewClientConnectedMessage struct {
-	message_id MessageID
-	client     ClientInfo
-}
-
-type ClientDisconnectedMessage struct {
-	message_id MessageID
-	name_len   byte
-	name       string
-}
-
-type BroadcastMessage struct {
-	message_id  MessageID
-	message_len uint16
-	message     string
-}
 
 const port = 7777
 
-var client_list map[net.Conn]ClientInfo = make(map[net.Conn]ClientInfo, 16)
-var client_list_mutex sync.Mutex
+var (
+	client_list       map[net.Conn]messages.ClientInfo = make(map[net.Conn]messages.ClientInfo, 16)
+	client_list_mutex sync.Mutex
+)
 
 func main() {
 	if len(os.Args) == 1 {
@@ -89,14 +25,12 @@ func main() {
 
 	// Resolve the string address to a TCP address
 	tcpAddr, err := net.ResolveTCPAddr("tcp4", fmt.Sprintf("localhost:%d", port))
-
 	if err != nil {
 		panic(err)
 	}
 
 	// Start listening for TCP connections on the given address
 	listener, err := net.ListenTCP("tcp", tcpAddr)
-
 	if err != nil {
 		panic(err)
 	}
@@ -113,119 +47,6 @@ func main() {
 	}
 }
 
-func UnmarshalRegistrationRequestMessage(conn io.Reader) (RegistrationRequestMessage, ErrorCode) {
-	m := make([]byte, 8)
-	_, err := conn.Read(m[:1])
-	if err != nil {
-		panic(err)
-	}
-
-	if m[0] != byte(RegistrationRequest) {
-		return RegistrationRequestMessage{}, InvalidMessageID
-	}
-
-	_, err = conn.Read(m[1:5])
-	if err != nil {
-		panic(err)
-	}
-
-	_, err = conn.Read(m[5:7])
-	if err != nil {
-		panic(err)
-	}
-
-	_, err = conn.Read(m[7:8])
-	if err != nil {
-		panic(err)
-	}
-
-	if m[7] == 0 {
-		return RegistrationRequestMessage{}, NameLengthZero
-	}
-
-	name := make([]byte, m[7])
-	_, err = conn.Read(name)
-	if err != nil {
-		panic(err)
-	}
-
-	if !utf8.Valid(name) {
-		return RegistrationRequestMessage{}, NameNotUTF8
-	}
-
-	return RegistrationRequestMessage{
-		message_id: MessageID(m[0]),
-		client: ClientInfo{
-			client_ip:   m[1:5],
-			client_port: binary.BigEndian.Uint16(m[5:7]),
-			name_len:    m[7],
-			name:        string(name),
-		},
-	}, NoError
-}
-
-func WriteClientInfo(w io.Writer, info ClientInfo) {
-	w.Write(info.client_ip)
-	binary.Write(w, binary.BigEndian, info.client_port)
-	binary.Write(w, binary.BigEndian, info.name_len)
-	binary.Write(w, binary.BigEndian, []byte(info.name))
-}
-
-func WriteRegistrationResponse(w io.Writer, list map[net.Conn]ClientInfo) {
-	binary.Write(w, binary.BigEndian, byte(RegistrationResponse))
-	binary.Write(w, binary.BigEndian, uint32(len(list)))
-
-	for _, info := range list {
-		WriteClientInfo(w, info)
-	}
-}
-
-func WriteBroadcastMessage(w io.Writer, msg string) {
-	binary.Write(w, binary.BigEndian, byte(Broadcast))
-	binary.Write(w, binary.BigEndian, uint32(len(msg)))
-	w.Write([]byte(msg))
-}
-
-// already read message ID
-func ReadBroadcastMessage(r io.Reader) BroadcastMessage {
-	msg_len := make([]byte, 4)
-	_, err := r.Read(msg_len)
-	if err != nil {
-		panic(err)
-	}
-
-	msg_len_16 := binary.BigEndian.Uint16(msg_len)
-	msg := make([]byte, msg_len_16)
-	_, err = r.Read(msg)
-	if err != nil {
-		panic(err)
-	}
-
-	return BroadcastMessage{
-		message_id:  Broadcast,
-		message_len: msg_len_16,
-		message:     string(msg),
-	}
-}
-
-func WriteClientDisconnectMessage(w io.Writer, name string) {
-	binary.Write(w, binary.BigEndian, byte(ClientDisconnectedS2C))
-	binary.Write(w, binary.BigEndian, byte(len(name)))
-	w.Write([]byte(name))
-}
-
-func WriteNewClientConnectedMessage(w io.Writer, client ClientInfo) {
-	binary.Write(w, binary.BigEndian, byte(NewClientConnected))
-	WriteClientInfo(w, client)
-}
-
-func writeError(w io.Writer, code ErrorCode) {
-	_, err := w.Write([]byte{byte(Error), byte(code)})
-	if err != nil {
-		panic(err)
-	}
-}
-
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
 
@@ -235,16 +56,16 @@ func handleConnection(conn net.Conn) {
 		}
 	}()
 
-	regReq, errCode := UnmarshalRegistrationRequestMessage(conn)
-	if errCode != NoError {
-		writeError(conn, errCode)
+	regReq, errCode := messages.ReadRegistrationRequestMessage(conn)
+	if errCode != messages.NoError {
+		messages.WriteErrorMessage(conn, errCode)
 		return
 	}
 
 	client_list_mutex.Lock()
-	client_list[conn] = regReq.client
+	client_list[conn] = regReq.Client
 
-	WriteRegistrationResponse(conn, client_list)
+	messages.WriteRegistrationResponse(conn, client_list)
 
 	client_list_mutex.Unlock()
 
@@ -254,7 +75,7 @@ func handleConnection(conn net.Conn) {
 		panic(err)
 	}
 
-	if res[0] != byte(ClientListRecieved) {
+	if res[0] != byte(messages.ClientListRecieved) {
 		fmt.Printf("Expected ClientListRecieved (3) but got %d\n", res[0])
 		return
 	}
@@ -266,7 +87,7 @@ func handleConnection(conn net.Conn) {
 			continue
 		}
 
-		WriteNewClientConnectedMessage(client_conn, regReq.client)
+		messages.WriteNewClientConnectedMessage(client_conn, regReq.Client)
 	}
 
 	client_list_mutex.Unlock()
@@ -284,17 +105,17 @@ func handleConnection(conn net.Conn) {
 			panic(err)
 		}
 
-		switch MessageID(msg_id[0]) {
-		case Broadcast:
-			msg := ReadBroadcastMessage(conn)
+		switch messages.MessageID(msg_id[0]) {
+		case messages.Broadcast:
+			msg := messages.ReadBroadcastMessage(conn)
 			client_list_mutex.Lock()
 
 			for client_conn := range client_list {
-				WriteBroadcastMessage(client_conn, msg.message)
+				messages.WriteBroadcastMessage(client_conn, msg.Message)
 			}
 
 			client_list_mutex.Unlock()
-		case ClientDisconnectedS2C:
+		case messages.ClientDisconnectedS2C:
 			client_list_mutex.Lock()
 
 			for client_conn := range client_list {
@@ -302,12 +123,12 @@ func handleConnection(conn net.Conn) {
 					continue
 				}
 
-				WriteClientDisconnectMessage(client_conn, regReq.client.name)
+				messages.WriteClientDisconnectMessage(client_conn, regReq.Client.Name)
 			}
 
 			client_list_mutex.Unlock()
 		default:
-			writeError(conn, InvalidMessageID)
+			messages.WriteErrorMessage(conn, messages.InvalidMessageID)
 		}
 	}
 }
